10a45b18ec738a48ad592ffda68f56ca5c418493
Aditi1-97 <Richha27@gmail.com>
2025-12-09 18:15:44 +0530

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+# python basic syntax
+print("Hey we are learning Python")
+
+name = "codingal"
+grade = 10
+marks = 80
+
+print("Hey my name is", name , "I read in", grade, "in science I got", marks)
+print(f"Hey my name is {name} I read in {grade} in science i got{marks}")
+
+
+
+if marks > 75:
+    print("Hey you passed")
+    if(marks > 79):
+        print("you got A+")
+
+else:
+    print("You failed")        
