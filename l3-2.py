22ea53c268a9dfde5a60ec632a963ce61bf2cf39
Aditi1-97 <Richha27@gmail.com>
2025-12-16 18:00:38 +0530

    Add files via upload

---
+++ l3-2.py
@@ -0,0 +1,10 @@
+number = int(input("Enter your number: "))
+
+if number >= 50:
+    if number%2 == 0:
+        print("Even number")
+    else:    
+        print("Odd number")
+
+else:
+    print("Your number is less than 50")        
