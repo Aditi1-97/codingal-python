93700d7144350391269a99f3e501d85bb8b84b37
Aditi1-97 <Richha27@gmail.com>
2026-01-01 17:57:34 +0530

    Add files via upload

---
+++ l7-2.py
@@ -0,0 +1,9 @@
+students = {"Alex" : 85, "Harry": 90, "John": 78}
+
+students["Messi"] = 83
+print(students)
+
+students["Harry"] = 95
+print(students)
+
+print("The marks of Alex is : ", students["Alex"])
