a0c70ba8cd131dae6a6d8f423c00fff4830ec877
Aditi1-97 <Richha27@gmail.com>
2026-01-01 17:47:25 +0530

    Add files via upload

---
+++ lesson7.py
@@ -0,0 +1,22 @@
+fruits = ["Mango", "Grapes", "Apple", "Orange", "Banana", "Kiwi"]
+
+fruits.append("Dragon Fruit")
+print(fruits)
+
+fruits.remove("Kiwi")
+print(fruits)
+
+fruits.pop(1)
+print(fruits)
+
+fruits.sort()
+print(fruits)
+
+fruits.reverse()
+print(fruits)
+
+slice = fruits[1:4]
+print(slice)
+
+slice.clear()
+print(slice)
