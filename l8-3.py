953442fe494fdecd7e2a48a4e8f426554760e12b
Aditi1-97 <Richha27@gmail.com>
2026-01-06 18:08:00 +0530

    Add files via upload

---
+++ l8-3.py
@@ -0,0 +1,8 @@
+fruits1 = {"Apple", "Mango", "Banana", "Orange", "Kiwi"}
+fruits2 = {"Kiwi", "Mango", "Dragon Fruit", "Orange", "Coconut"}
+
+set1 = fruits1.union(fruits2)
+print(set1)
+
+set2 = fruits1.intersection(fruits2)
+print(set2)
