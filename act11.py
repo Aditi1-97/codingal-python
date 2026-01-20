ae0b1ef39f271e42b3257fc42ec6a8348e1dc098
Aditi1-97 <Richha27@gmail.com>
2026-01-20 21:12:56 +0530

    Add files via upload

--- act11.py
+++ act11.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
-import math
 
 
+# Abstract base class
 class Polygon(ABC):
 
     @abstractmethod
@@ -9,6 +9,7 @@ class Polygon(ABC):
         pass
 
 
+# Rectangle class
 class Rectangle(Polygon):
     def __init__(self, length, breadth):
         self.length = length
@@ -18,7 +19,7 @@ class Rectangle(Polygon):
         return self.length * self.breadth
 
 
-
+# Square class
 class Square(Polygon):
     def __init__(self, side):
         self.side = side
@@ -27,6 +28,7 @@ class Square(Polygon):
         return self.side * self.side
 
 
+# Triangle class
 class Triangle(Polygon):
     def __init__(self, base, height):
         self.base = base
@@ -36,43 +38,30 @@ class Triangle(Polygon):
         return 0.5 * self.base * self.height
 
 
-
-class Circle(Polygon):
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return math.pi * self.radius * self.radius
-
-
-
+# Menu-driven program
+print("Choose a Polygon to Calculate Area")
 print("1. Rectangle")
 print("2. Square")
 print("3. Triangle")
-print("4. Circle")
 
 choice = int(input("Enter your choice: "))
 
 if choice == 1:
     l = float(input("Enter length: "))
     b = float(input("Enter breadth: "))
-    obj = Rectangle(l, b)
+    shape = Rectangle(l, b)
 
 elif choice == 2:
     s = float(input("Enter side: "))
-    obj = Square(s)
+    shape = Square(s)
 
 elif choice == 3:
     base = float(input("Enter base: "))
     height = float(input("Enter height: "))
-    obj = Triangle(base, height)
-
-elif choice == 4:
-    r = float(input("Enter radius: "))
-    obj = Circle(r)
+    shape = Triangle(base, height)
 
 else:
-    print("Invalid choice")
+    print("Invalid choice!")
     exit()
 
-print("Area =", obj.area())
+print("Area =", shape.area())
