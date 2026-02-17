d9f7539c3f850fbfe1f82d75796b86e03b91da6a
Aditi1-97 <Richha27@gmail.com>
2026-02-17 18:24:59 +0530

    Add files via upload

---
+++ tkin4.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+# setting up main window
+root = Tk()
+root.geometry("400x300")
+root.title("main")
+
+# function to open new (top level) window
+def topwin():
+    # setting up top window
+    top = Toplevel()
+    top.geometry("180x100")
+    top.title("toplevel")
+
+    # adding a label widget to top window
+    l2 = Label(top, text="This is toplevel window")
+    l2.pack()
+
+# adding a label and button widget to root (main) window
+l = Label(root, text="This is root window")
+btn = Button(root, text="Click here to open another window", command=topwin)
+
+# arranging widgets
+l.pack()
+btn.pack()
+
+root.mainloop()
