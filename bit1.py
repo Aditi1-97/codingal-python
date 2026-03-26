5efe47c25baf755b32939537777fa0758f704c5b
Aditi1-97 <Richha27@gmail.com>
2026-03-26 18:02:23 +0530

    Add files via upload

---
+++ bit1.py
@@ -0,0 +1,26 @@
+num1 = 10
+num2 = 4
+# using AND operator
+print("num1 & num2 =", num1 & num2)
+# using OR operator
+print("\nnum1 | num2 =", num1 | num2)
+# using NOT operator
+print("\n~num1 =", ~num1)
+# using XOR operator
+print("\nnum1 ^ num2 =", num1^ num2)
+
+num1 = 10
+num2 = 4
+
+# using Right shift operator on num1
+print("\nnum1 >> 1 =", num1 >> 1)
+# using Right shift operator on num2
+print("\nnum2 >> 1 =", num2 >> 1)
+
+num1 = 10
+num2 = 4
+
+# using Left shift operator on num1
+print("\nnum1 << 1 =", num1 << 1)
+# using Left shift operator on num2
+print("\nnum2 << 1 =", num2 << 1)
