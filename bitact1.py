a8d362d9e3ec4b897802569320ce338dda2f27dd
Aditi1-97 <Richha27@gmail.com>
2026-03-28 23:34:16 +0530

    Add files via upload

---
+++ bitact1.py
@@ -0,0 +1,10 @@
+A = int(input("Enter value of A: "))
+B = int(input("Enter value of B: "))
+C = int(input("Enter value of C: "))
+
+if B == 1 and (A == 1 or C ==1):
+    Q = 1
+else:
+    Q = 0
+
+print("Output Q = ", Q)        
